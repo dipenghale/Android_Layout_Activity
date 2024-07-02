663ef131e0d7f02e6cadb5cb6350a1debd7c1eb1
Dipen Ghale <dipenghale@gmail.com>
2024-07-02 17:52:54 +0545

    New updates

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
@@ -16,14 +17,17 @@ MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        Log.d((String) "MainActivity", "onCreate");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         EditText etName = findViewById(R.id.etName);
+        EditText etEmail = findViewById(R.id.etEmail);
 
         Button btn = findViewById(R.id.regButton);
         btn.setOnClickListener(v -> {
             String name = etName.getText().toString();
+            String email = etEmail.getText().toString();
             if(name.isEmpty()) {
                 //DO SOMETHING! {RUN SOME FUNCTION ... DO CHECKS... ETC}
                 Toast toast = Toast.makeText(this,"Full Name is empty.",Toast.LENGTH_LONG);
@@ -31,6 +35,8 @@ MainActivity extends AppCompatActivity {
             }
             else {
                 Intent intent = new Intent(this,SecondActivity.class);
+                intent.putExtra("full_name",name);
+                intent.putExtra("email_address",email);
                 startActivity(intent);
             }
         });
@@ -50,4 +56,41 @@ MainActivity extends AppCompatActivity {
 
 
     }
+
+    @Override
+    public void onStart(){
+        super.onStart();
+        Log.d((String) "MainActivity", "onStart");
+    }
+
+    @Override
+    public void onPause(){
+        super.onPause();
+        Log.d((String) "MainActivity", "onPause");
+    }
+
+    @Override
+    public void onResume(){
+        super.onResume();
+        Log.d((String) "MainActivity", "onResume");
+    }
+
+    @Override
+    public void onStop(){
+        super.onStop();
+        Log.d((String) "MainActivity", "onStop");
+    }
+
+    @Override
+    public void onDestroy(){
+        super.onDestroy();
+        Log.d((String) "MainActivity", "onDestroy");
+    }
+
+    @Override
+    public void onRestart(){
+        super.onRestart();
+        Log.d((String) "MainActivity", "onRestart");
+    }
+
 }
--- app/src/main/java/com/example/myapplication/SecondActivity.java
+++ app/src/main/java/com/example/myapplication/SecondActivity.java
@@ -3,6 +3,7 @@ package com.example.myapplication;
 import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
@@ -15,6 +16,19 @@ public class SecondActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
 
+        TextView display_fullname = findViewById(R.id.display_fullname);
+        TextView display_email = findViewById(R.id.display_email);
+
+        //Receive data sent from Main Activity
+        Bundle bundle = getIntent().getExtras();
+        String full_name = bundle.getString("full_name");
+        String email_address = bundle.getString("email_address");
+
+        //set received data to textview in layout
+        display_fullname.setText(full_name);
+        display_email.setText(email_address);
+
+        //Go to Main activity after button clicked
         Button btn = findViewById(R.id.gotothird);
         btn.setOnClickListener(v -> {
 
